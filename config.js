fd6d7eb723cd4a06b802c444c8116416f4878e0f
R Holder <59735305+alienpoodle@users.noreply.github.com>
2025-07-05 16:02:23 -0400

    added config, manifest and ico

---
+++ config.js
@@ -0,0 +1,12 @@
+window.firebaseConfig = {
+    apiKey: "AIzaSyBl6-Cfz8fvgxWcExEdqEHQ6NsMNLIT0AQ",
+    authDomain: "hitchpoint-fabef.firebaseapp.com",
+    projectId: "hitchpoint-fabef",
+    storageBucket: "hitchpoint-fabef.firebasestorage.app",
+    messagingSenderId: "662205524100",
+    appId: "1:662205524100:web:73c4c532c16fcaceee7ad1",
+    measurementId: "G-SBKJNWMBNM",
+    googleMapsApiKey: "AIzaSyCOBPhsHs2BEdz7DTEofxUxlWKPCzY3Qkk" 
+};
+
+            
